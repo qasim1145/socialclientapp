0ddf9fcfddcc115796590dab8b71f544f5fa4bfc
qasim1145 <cmqasim123@gmail.com>
2023-07-20 14:11:50 -0700

    created get req

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,37 @@
-import { Component } from '@angular/core';
+import { Component,OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'social-app';
+  users: any = [];
+  constructor(private http: HttpClient) {
+  
+  }
+
+  ngOnInit(): void {
+  
+   this.getActiveusers();
+  }
+
+  getActiveusers() {
+
+    this.http.get<any>("https://localhost:44322/api/User/GetAllActiveUsers").subscribe(
+      {
+        next: (response) => {
+          this.users = response.data;
+          console.log(response);
+        },
+        error: (e) => console.error(e)
+      }
+     )
+
+  }
+  
 }
